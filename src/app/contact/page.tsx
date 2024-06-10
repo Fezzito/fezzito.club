58f887c9f4933b6809ad2213a788b9c7c854df97
Fezzito <ftzeitler@gmail.com>
2024-06-10 10:27:22 -0300

    background fixed, starting Head component with metadata from NextJS

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,28 +12,35 @@ import {
 import { mySocials } from "@/lib/constants";
 import { Bebas_Neue } from "next/font/google";
 import "tailwindcss/tailwind.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+};
 
 const bebas_neue = Bebas_Neue({ subsets: ["latin"], weight: "400" });
 
 export default function Page() {
   return (
-    <WorkWrapper className="contact">
-      <SectionTitle className={bebas_neue.className}>Contact</SectionTitle>
-      <GoBack href={"/"}>{"<<<back"}</GoBack>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 grid-rows-2 auto-rows-[1fr] ">
-        {mySocials.map((item, index) => (
-          <Card
-            key={index}
-            onClick={() => {
-              window.open(item.url, "_blank");
-            }}
-            className="fromContact"
-          >
-            <AboutText fontSize="15px">{item.title}</AboutText>
-            <ImageComponent src={item.imagen} />
-          </Card>
-        ))}
-      </div>
-    </WorkWrapper>
+    <>
+      <WorkWrapper className="contact">
+        <SectionTitle className={bebas_neue.className}>Contact</SectionTitle>
+        <GoBack href={"/"}>{"<<<back"}</GoBack>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 grid-rows-2 auto-rows-[1fr] ">
+          {mySocials.map((item, index) => (
+            <Card
+              key={index}
+              onClick={() => {
+                window.open(item.url, "_blank");
+              }}
+              className="fromContact"
+            >
+              <AboutText fontSize="15px">{item.title}</AboutText>
+              <ImageComponent src={item.imagen} />
+            </Card>
+          ))}
+        </div>
+      </WorkWrapper>
+    </>
   );
 }
